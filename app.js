013169bca084794bd44ee935fb7036da8caa61c2
meriamatt <mer.attaiki@gmail.com>
2022-04-12 16:19:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://MeriamAtt:Benammar20@meriamattcluster.3mqot.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+  app.use('/api/auth', userRoutes);
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a très bien été reçue !' }); 
+});
+
+module.exports = app;
