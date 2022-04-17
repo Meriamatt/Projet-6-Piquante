e21dc630df084ece20e914471aadddff87b4ea32
meriamatt <mer.attaiki@gmail.com>
2022-04-17 13:23:16 +0200

    express-rate-limit add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauces');
 const path = require('path');
 const helmet = require('helmet');
-
+const rateLimit = require('express-rate-limit');
 
 
 
@@ -19,14 +19,32 @@ mongoose.connect(`mongodb+srv://${process.env.DB_SRV_ADDRESS}:${process.env.DB_P
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(helmet({
+  ccrossOriginResourcePolicy: {
+    policy: "same-site"
+  }
+}));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 //CORS authorization
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
   next();
 });
+
+
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
