963d2ea7df253e486765a7aad62c90bd9748f595
meriamatt <mer.attaiki@gmail.com>
2022-04-29 15:15:49 +0200

    add limitRate to roots

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
+const limitRate = require ('../middleware/limit-rate');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+
+router.post('/signup', limitRate, userCtrl.signup);
+router.post('/login', limitRate, userCtrl.login);
 
 module.exports = router;
 
